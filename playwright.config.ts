d0f3ea59b74dda272d8b8558f68665337200979f
Alex <138037691+chernyal@users.noreply.github.com>
2023-10-15 15:16:02 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,3 +52,5 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
   actionTimeout: 10000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+ trace: 'on-first-retry',
+    baseURL: 'http://localhost:3333/',
