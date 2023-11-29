9578a27505c93c011b67784115408ad4445b4144
Alex <138037691+chernyal@users.noreply.github.com>
2023-11-29 15:48:39 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,25 +6,3 @@ import os from 'os'
 import path from 'path'
 import {defineConfig, devices} from '@playwright/test'
 
-
-// Paths
-const TESTS_PATH = path.join(__dirname, 'test', 'e2e', 'tests')
-const HTML_REPORT_PATH = path.join(__dirname, 'test', 'e2e', 'report')
-const ARTIFACT_OUTPUT_PATH = path.join(__dirname, 'test', 'e2e', 'results')
-
-// OS-specific browsers to include
-const OS_BROWSERS =
-  os.platform() === 'darwin' ? [{name: 'webkit', use: {...devices['Desktop Safari']}}] : []
-
-// Read environment variables
-const CI = readBoolEnv('CI', false)
-const E2E_DEBUG = readBoolEnv('SANITY_E2E_DEBUG', false)
-const PROJECT_ID = process.env.SANITY_E2E_PROJECT_ID!
-
-const BASE_URL = 'http://localhost:3339/'
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-export default defineConfig({
-globalSetup: require.resolve('./test/e2e/globalSetup'),
