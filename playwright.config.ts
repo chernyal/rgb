54cd44f7e79060366971140c7f1cd2c90a3a1ecf
Alex <138037691+chernyal@users.noreply.github.com>
2023-12-07 13:58:17 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,10 +18,4 @@ const OS_BROWSERS =
 
 // Read environment variables
 const CI = readBoolEnv('CI', false)
-const E2E_DEBUG = readBoolEnv('SANITY_E2E_DEBUG', false)
-const PROJECT_ID = process.env.SANITY_E2E_PROJECT_ID!
 
-const BASE_URL = 'http://localhost:3339/'
-
-/**
- * See https://playwright.dev/docs/test-configuration.
